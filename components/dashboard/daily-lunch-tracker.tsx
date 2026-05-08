fbd6edc3b0fcf0611f3272979494cacf5e9bd958
shfqtm2 <shafqat.ullah@mobify.com.pk>
2026-05-08 15:46:25 +0500

    space

--- components/dashboard/daily-lunch-tracker.tsx
+++ components/dashboard/daily-lunch-tracker.tsx
@@ -10,6 +10,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { Input } from "@/components/ui/input"
